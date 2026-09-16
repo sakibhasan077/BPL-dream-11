9e55c25c7920bb07e55ba852e48e8e94318ce77e
SakibHasan077 <sakibhasanmd077@gmail.com>
2026-09-16 23:53:08 +0600

    Add logo and dollar icons; refine Navbar layout

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -5,44 +5,32 @@ import Logo from "../../assets/image/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 z-50 w-full bg-white py-2 shadow">
-      <div className="container mx-auto flex items-center justify-between">
-
+    <nav className="fixed top-0 z-50 bg-white shadow w-full py-2">
+      <div className="container mx-auto flex justify-between items-center ">
         <div>
           <img src={Logo} alt="Website Logo" />
         </div>
-
-        <div className="flex items-center gap-18">
-
+        <div className="flex gap-18 items-center">
           <ul className="flex gap-8 text-[#131313]">
             <li>
               <a href="#">Home</a>
             </li>
-
             <li>
               <a href="#">Fixture</a>
             </li>
-
             <li>
               <a href="#">Teams</a>
             </li>
-
             <li>
               <a href="#">Schedules</a>
             </li>
           </ul>
-
           <div>
-            <button className="flex items-center gap-1.5 rounded-[10px] border-2 border-[#e7e7e7] px-5 py-4 text-[17px] font-black">
-              <img
-                src={DollarIcon}
-                className="h-5 w-5"
-                alt="Dollar Icon"
-              />
+            <button className="flex gap-1.5 items-center border-2 rounded-[10px] border-[#e7e7e7] px-5 py-4 font-black text-[17px] ">
+              <img src={DollarIcon} className="w-5 h-5" alt="Dollar Icon " />
               0
             </button>
           </div>
-
         </div>
       </div>
     </nav>
